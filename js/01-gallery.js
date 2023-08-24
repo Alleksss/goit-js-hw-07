89a56c530059328477d5544d8c64ac8ff50d91fb
Alleksss <Solidnuii@gmail.com>
2023-08-24 23:38:23 +0300

    feat: decision

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', openLargeImage);
+gallery.append(...genGallaryItems(galleryItems));
+
+function openLargeImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG')
+        return;
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}">`
+    );
+
+    instance.show();   
+};
+
+function genGallaryItems(arr) {
+    return arr.map(
+        i => {
+            const item = document.createElement('li');
+            item.className = 'gallery__item';
+            const a = document.createElement('a');
+            a.className = 'gallery__link';
+            a.href = i.original;
+            const img = document.createElement('img');
+            img.className = 'gallery__image';
+            img.src = i.preview;
+            img.dataset.source = i.original;
+            img.alt = i.description;
+            a.append(img);
+            item.append(a);
+            return item;
+        }
+    );
+};
+
+
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+// gallery.addEventListener("click", openLargeImage);
+
+gallery.insertAdjacentHTML('afterbegin', genGallaryItems(galleryItems));
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
+
+function genGallaryItems(arr) {
+  return arr.map(
+      ({ original, preview, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    ).join("");
+}
 
 console.log(galleryItems);
